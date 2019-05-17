0e5280d4c0289e71057971de5464b8e7b265a1e0
chengchuangyue <1132265836@qq.com>
2019-05-17 15:21:51 +0800

    change

--- src/main/java/web/IndexController.java
+++ src/main/java/web/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/index")
     public String index(){
-        String re = "sdsd";
+
         return "index";
     }
 
